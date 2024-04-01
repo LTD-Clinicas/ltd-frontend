5a7b4f3ff0fc81f9173e7f9a30b5895e4e00dfb9
ltd-psico <ltdclinicas.2024@gmail.com>
2024-04-01 18:16:32 -0300

    .

--- app/(ROTAS)/login/page.tsx
+++ app/(ROTAS)/login/page.tsx
@@ -29,60 +29,53 @@ export default function Login() {
                         </h2>
                     </div>
 
-                    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                        <form className="space-y-6">
-                            <section>
-                                <Label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Email ou Matrícula
-                                </Label>
-                                <Input
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    required
-                                    className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                                />
-                            </section>
+                    <form className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm space-y-6">
+                        <section>
+                            <Label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email ou Matrícula
+                            </Label>
 
-                            <section>
-                                <div className="flex items-center justify-between">
-                                    <Label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                        Senha
-                                    </Label>
+                            <Input
+                                onChange={(e) => setEmail(e.target.value)}
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                            />
 
-                                    <a href="#" className="text-sm font-semibold text-blue-400 hover:text-blue-500">
-                                        Esqueceu sua senha?
-                                    </a>
-                                </div>
+                            <Label htmlFor="password" className="flex items-center justify-between text-sm font-medium leading-6 text-gray-900">
+                                Senha
+                                <a href="#" className="text-sm font-semibold text-blue-400 hover:text-blue-500">
+                                    Esqueceu sua senha?
+                                </a>
+                            </Label>
 
-                                <Input
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    required
-                                    className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
-                                />
-                            </section>
+                            <Input
+                                onChange={(e) => setPassword(e.target.value)}
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
+                            />
+                        </section>
 
-                            <Button
-                                onClick={handleLogin}
-                                className="flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
-                            >
-                                Entrar
-                            </Button>
-                        </form>
-
+                        <Button
+                            onClick={handleLogin}
+                            className="flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+                        >
+                            Entrar
+                        </Button>
                         <span className="mt-10 text-center text-sm text-gray-900">
                             Não tem cadastro?{' '}
                             <a href="#" className="font-semibold leading-6 text-blue-400 hover:text-indigo-500">
                                 Clique aqui
                             </a>
                         </span>
-                    </div>
+                    </form>
                 </CardContent>
             </Card>
         </main>
