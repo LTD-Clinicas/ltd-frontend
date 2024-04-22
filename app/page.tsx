efc4452e9ec7113cd0ca3faf810c0678db753acc
ltd-psico <ltdclinicas.2024@gmail.com>
2024-04-22 16:21:10 -0300

    criação pagina de cadastro de clinica

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react"
 
+
 export default function Home() {
     return (
-        <>
-        </>    
+        <div className="flex min-h-screen flex-col items-center justify-center p-24">
+            <h1 className="text-3xl font-bold underline">PAGINA DE LOGIN</h1>
+            (EM DESENVOLVIMENTO)
+            <a className="font-bold text-2xl" href="/login">Login</a>
+        </div>
     )
 }
