686701cdde527700b60579f9663ff772d48cdf6c
Lucas Henrique <lucas.henricruz@gmail.com>
2024-04-22 16:42:52 -0300

    update

--- app/(ROTAS)/clinica/page.tsx
+++ app/(ROTAS)/clinica/page.tsx
@@ -13,15 +13,15 @@ import {
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
 const products = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+  { name: 'Exemplo1', description: '----------', href: '#', icon: ChartPieIcon },
+  { name: 'Exemplo2', description: '----------', href: '#', icon: CursorArrowRaysIcon },
+  { name: 'Exemplo3', description: '----------', href: '#', icon: FingerPrintIcon },
+  { name: 'Exemplo4', description: '----------', href: '#', icon: SquaresPlusIcon },
+  { name: 'Exemplo5', description: '----------', href: '#', icon: ArrowPathIcon },
 ]
 const callsToAction = [
   { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+  { name: 'Contatos', href: '#', icon: PhoneIcon },
 ]
 
 function classNames(...classes) {
@@ -200,4 +200,5 @@ export default function Example() {
       </Dialog>
     </header>
   )
+  
 }
