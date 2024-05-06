37379c0686740cb5f28f5a4e81d1d1d705a0e8f7
jonatasPinheiroSilva <contatopessoalbolota@gmail.com>
2024-05-06 17:45:10 -0300

    tela de cadastro

--- app/(ROTAS)/cadastroPaciente/page.tsx
+++ app/(ROTAS)/cadastroPaciente/page.tsx
@@ -10,15 +10,11 @@ export default function SignUp() {
     const [email, setEmail] = useState("")
     const [cpf, setCpf] = useState("")
     const [dataNascimento, setDataNascimento] = useState("")
-    const [enderecoEstado, setEnderecoEstado] = useState("")
-    const [enderecoCidade, setEnderecoCidade] = useState("")
-    const [enderecoBairro, setEnderecoBairro] = useState("")
-    const [enderecoNumero, setEnderecoNumero] = useState("")
     const [senha, setSenha] = useState("")
     const [confirmaSenha, setConfirmaSenha] = useState("")
 
     const handleSignUp = () => {
-        console.log("Cadastro: Nome Completo: " + nomeCompleto + ", Email: " + email + ", CPF: " + cpf + ", Data de Nascimento: " + dataNascimento + ", Estado: " + enderecoEstado + ", Cidade: " + enderecoCidade + ", Bairro: " + enderecoBairro + ", Número: " + enderecoNumero + ", Senha: " + senha)
+        console.log("Cadastro: Nome Completo: " + nomeCompleto + ", Email: " + email + ", CPF: " + cpf + ", Data de Nascimento: " + dataNascimento + ", Senha: " + senha)
     }
 
     return (
@@ -42,7 +38,6 @@ export default function SignUp() {
                                 id="nomeCompleto"
                                 name="nomeCompleto"
                                 type="text"
-                                autoComplete="nome"
                                 required
                                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                             />
@@ -56,8 +51,6 @@ export default function SignUp() {
                                 id="email"
                                 name="email"
                                 type="email"
-                                autoComplete="email"
-                                required
                                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                             />
 
@@ -70,8 +63,6 @@ export default function SignUp() {
                                 id="cpf"
                                 name="cpf"
                                 type="text"
-                                autoComplete="cpf"
-                                required
                                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                             />
 
@@ -84,63 +75,6 @@ export default function SignUp() {
                                 id="dataNascimento"
                                 name="dataNascimento"
                                 type="date"
-                                required
-                                className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                            />
-
-                            <Label htmlFor="estado" className="block text-sm font-medium leading-6 text-gray-900">
-                                Estado
-                            </Label>
-
-                            <Input
-                                onChange={(e) => setEnderecoEstado(e.target.value)}
-                                id="estado"
-                                name="estado"
-                                type="text"
-                                autoComplete="estado"
-                                required
-                                className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                            />
-
-                            <Label htmlFor="cidade" className="block text-sm font-medium leading-6 text-gray-900">
-                                Cidade
-                            </Label>
-
-                            <Input
-                                onChange={(e) => setEnderecoCidade(e.target.value)}
-                                id="cidade"
-                                name="cidade"
-                                type="text"
-                                autoComplete="cidade"
-                                required
-                                className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                            />
-
-                            <Label htmlFor="bairro" className="block text-sm font-medium leading-6 text-gray-900">
-                                Bairro
-                            </Label>
-
-                            <Input
-                                onChange={(e) => setEnderecoBairro(e.target.value)}
-                                id="bairro"
-                                name="bairro"
-                                type="text"
-                                autoComplete="bairro"
-                                required
-                                className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-                            />
-
-                            <Label htmlFor="numero" className="block text-sm font-medium leading-6 text-gray-900">
-                                Número
-                            </Label>
-
-                            <Input
-                                onChange={(e) => setEnderecoNumero(e.target.value)}
-                                id="numero"
-                                name="numero"
-                                type="text"
-                                autoComplete="endereco"
-                                required
                                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                             />
 
@@ -153,7 +87,6 @@ export default function SignUp() {
                                 id="senha"
                                 name="senha"
                                 type="password"
-                                autoComplete="nova-senha"
                                 required
                                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                             />
@@ -167,7 +100,6 @@ export default function SignUp() {
                                 id="confirmaSenha"
                                 name="confirmaSenha"
                                 type="password"
-                                autoComplete="nova-senha"
                                 required
                                 className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
                             />
--- app/(ROTAS)/cadastroProfessor/page.tsx
+++ app/(ROTAS)/cadastroProfessor/page.tsx
@@ -52,7 +52,6 @@ export default function SignUp() {
                                 name="email"
                                 type="email"
                                 autoComplete="email"
-                                required
                                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                             />
 
@@ -66,7 +65,6 @@ export default function SignUp() {
                                 name="cpf"
                                 type="text"
                                 autoComplete="cpf"
-                                required
                                 className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                             />
 
