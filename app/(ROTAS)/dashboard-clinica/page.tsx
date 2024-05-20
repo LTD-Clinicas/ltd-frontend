149b1da6fea8e00bdd9ded14b07aab827269e37e
Lucas Henrique <lucas.henricruz@gmail.com>
2024-05-20 18:03:23 -0300

    update 20/05 18:03
    update

--- app/(ROTAS)/dashboard-clinica/page.tsx
+++ app/(ROTAS)/dashboard-clinica/page.tsx
@@ -1,8 +1,5 @@
 "use client"
 import React, { useState } from 'react';
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Calendar } from "@/components/ui/calendar"
 import  Navbar  from "@/components/ui/navbar"
 
