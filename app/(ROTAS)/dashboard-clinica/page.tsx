e8d9cbe529540f393cec14664ba54e75b1abb0ae
Lucas Henrique <lucas.henricruz@gmail.com>
2024-05-25 16:12:03 -0300

    update
    update

--- app/(ROTAS)/dashboard-clinica/page.tsx
+++ app/(ROTAS)/dashboard-clinica/page.tsx
@@ -13,14 +13,6 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-const user = {
-  name: 'user',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-};
-
-
 export default function MarcarConsulta() {
   const [date, setDate] = useState<Date | undefined>(new Date());
   const [turno, setTurno] = useState("");
