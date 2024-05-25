48e1743b0b49404ba41fb35c9731884e4d0300ee
Lucas Henrique <lucas.henricruz@gmail.com>
2024-05-25 16:11:07 -0300

    atualização 25/05 16:10
    update

--- app/(ROTAS)/dashboard-clinica/page.tsx
+++ app/(ROTAS)/dashboard-clinica/page.tsx
@@ -20,23 +20,6 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 };
 
-const navigation = [
-  { name: 'exemplo', href: '#', current: true },
-  { name: 'exemplo', href: '#', current: false },
-  { name: 'exemplo', href: '#', current: false },
-  { name: 'exemplo', href: '#', current: false },
-  { name: 'exemplo', href: '#', current: false },
-];
-
-const userNavigation = [
-  { name: 'Seu perfil', href: '#' },
-  { name: 'configurações', href: '#' },
-  { name: 'sair', href: '#' },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
 
 export default function MarcarConsulta() {
   const [date, setDate] = useState<Date | undefined>(new Date());
