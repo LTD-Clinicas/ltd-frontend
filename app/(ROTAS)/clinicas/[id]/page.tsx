07d0215e31b7772da9e2ce253df7d0654256eec3
Marck-vsv <marck.vinisv@outlook.com>
2024-05-29 13:26:47 -0300

    Olha o commit

--- app/(ROTAS)/clinicas/[id]/page.tsx
+++ app/(ROTAS)/clinicas/[id]/page.tsx
@@ -1,87 +1,117 @@
-"use client"
+"use client";
+import React, { useState } from 'react';
+import { Calendar } from "@/components/ui/calendar";
 import Navbar from "@/components/ui/navbar";
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
-import { useState } from "react";
-import {parseCookies} from "nookies";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { parseCookies} from "nookies";
 
-export default function Clinica({
-                                    params,
-                                }: Readonly<{ params: { ID: string } }>) {
-    const { ID } = params;
+export default function MarcarConsulta({ params }: Readonly<{ params: { ID: string } }>) {
+    const [date, setDate] = useState<Date | undefined>(new Date());
+    const [showAlert, setShowAlert] = useState(false);
     const [arrClinicas, setArrClinicas] = useState({
-        nome: "",
-        descricao: "",
-        quantidadeMax: "",
         inicio: "",
         fim: "",
         inicio2: "",
         fim2: "",
-        diasAtendimento: "",
-        maxPorHorario: "",
+        quantidadeMax: "",
+        maxPorHorario: ""
     });
-    
+    const { ID } = params;
     const {'token': token} = parseCookies();
     
-    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/clinica/${ID}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
-    .then(async response => {
-        if (!response.ok) {
-            console.error("Erro na requisição")
-        }
-    return await response.json()})
-    .then(data => {
-        setArrClinicas(data)
-    }).catch(error => {
-        console.error(error)
-    })
+    console.log("Teste batata")
+
+    // fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/clinica/1`, {
+    //     headers: {
+    //         Authorization: `Bearer ${token}`
+    //     }
+    // })
+    //     .then(async response => {
+    //         if (!response.ok) {
+    //             console.error("Erro na requisição")
+    //         }
+    //         return await response.json()})
+    //     .then(data => {
+    //         setArrClinicas(data)
+    //     }).catch(error => {
+    //     console.error(error)
+    // })
     
+    const clinicaObj = {
+        horarioInicio1: arrClinicas.inicio,
+        horarioFim1: arrClinicas.fim,
+        
+        horarioInicio2: arrClinicas.inicio2,
+        horarioFim2: arrClinicas.fim2,
+        
+        quantidadeMax: arrClinicas.quantidadeMax,
+        maxPorTurno: arrClinicas.maxPorHorario,
+    }
+
     return (
-        <main>
-            <Navbar/>
-            
-            <div className="w-full">
-                <Card className={"p-10 w-1/2 mx-auto mt-10"}>
-                    <CardTitle className={"text-center"}>
-                        {arrClinicas.nome}
-                    </CardTitle>
+        <>
+            <div className="min-h-full">
+                <Navbar />
+                <header className="bg-white shadow">
+                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                        <h1 className="text-3xl font-bold tracking-tight text-gray-900">Consulta</h1>
+                    </div>
+                </header>
+                <main>
+                    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                        {showAlert && (
+                            <Alert>
+                                <AiOutlineExclamationCircle className="h-4 w-4" />
+                                <AlertTitle>Alerta!</AlertTitle>
+                                <AlertDescription>
 
-                    <CardContent>
-                        <div>
-                            <p>
-                                Descrição da Clinica: <span className={"text-blue-500"}>{arrClinicas.descricao}</span>
-                            </p>
+                                </AlertDescription>
+                            </Alert>
+                        )}
+                        <div className="flex items-center space-x-4">
+                            <Calendar
+                                mode="single"
+                                selected={date}
+                                onSelect={(value) => {setDate(value)}}
+                                className="rounded-md border"
+                            />
                             
-                            <p>
-                                Quantidade máxima de pacientes: <span className={"text-blue-500"}>{arrClinicas.quantidadeMax}</span>
-                            </p>
-                            
-                            <div>
-                                <p className={"flex flex-col justify-between p-4"}>
-                                    Horário de inicio: <span className={"text-blue-500"}>{arrClinicas.inicio}</span>
-                                    Horário de fim: <span className={"text-blue-500"}>{arrClinicas.fim}</span>
-                                </p>
-                            </div>
+                            <Card className="flex-1">
+                                <CardHeader>
+                                    <CardTitle>Marcar Consulta</CardTitle>
+                                    <CardDescription>
+                                        Marque o dia e o mês no calendário que deseja realizar a sua consulta!
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <div className="space-y-4">
+                                        <div>
+                                            <label htmlFor="turno" className="block text-sm font-medium text-gray-700">
+                                                Turno
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label htmlFor="horario" className="block text-sm font-medium text-gray-700">
+                                                Horário
+                                            </label>
 
-                            <div>
-                                <p className={"flex flex-col justify-between p-4"}>
-                                    Horário de inicio tarde: <span className={"text-blue-500"}>{arrClinicas.inicio2}</span>
-                                    Horário de fim tarde: <span className={"text-blue-500"}>{arrClinicas.fim2}</span>
-                                </p>
-                            </div>                         
-                            
-                            <div>
-                                <p className={"flex flex-col justify-between p-4"}>
-                                    Dias de atendimento na semana: <span className={"text-blue-500"}>{arrClinicas.diasAtendimento}</span>
-                                    Máximo de pacientes por turno: <span className={"text-blue-500"}>{arrClinicas.quantidadeMax}</span>
-                                </p>
-                            </div>
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </Card>
                         </div>
-                    </CardContent>
-                </Card>
+                    </div>
+                </main>
             </div>
-        </main>
-    )
+        </>
+    );
 }
